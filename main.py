27753be1d76a22f56cd637bfe03d7cd9cc6314ed
Maria Khan <mariapervaiz40@gmail.com>
2025-12-06 01:14:04 +0500

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import uvicorn
+import os
 from chatkit_server import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000))
+    )
